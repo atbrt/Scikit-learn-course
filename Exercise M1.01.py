be450d20bf2e4d06d64e68550f9b264e36037b4e
Arold Toubert <aroldtoubert@MacBook-Air-de-Arold.local>
2024-10-05 16:54:19 +0200

    Ajout

--- Exercise M1.01.py
+++ Exercise M1.01.py
@@ -1,3 +1,16 @@
+
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 penguins=pd.read_csv("bases de données/penguins_classification.csv")
+
+
+
+penguins.head()
+
+especes=penguins["Species"]
+especes.value_counts()
+penguinsnum=penguins.select_dtypes(include=['number'])
+_=penguinsnum.hist(figsize=(20, 14))
+plt.show()
 
