55a5a670753e8ba6e66fef4a4256081eb43e2fba
Arold Toubert <aroldtoubert@gmail.com>
2024-10-08 19:51:06 +0200

    New pipeline

--- Chapter 1/Exercise M1.05.py
+++ Chapter 1/Exercise M1.05.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import cross_validate
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import HistGradientBoostingClassifier
 from sklearn.compose import make_column_selector as selector
 
@@ -43,3 +44,35 @@ print(
 )
 
 #Second pipeline: scaling numerical features
+
+categorical_preprocessor = OrdinalEncoder(
+    handle_unknown="use_encoded_value", unknown_value=-1
+)
+numerical_preprocessor=StandardScaler()
+
+preprocessor = ColumnTransformer(
+    [
+        ("categorical", categorical_preprocessor, categorical_columns),
+        ("standard_scaler", numerical_preprocessor, numerical_columns),
+        
+        ]
+    
+)
+
+model = make_pipeline(preprocessor, HistGradientBoostingClassifier())
+
+start = time.time()
+cv_results = cross_validate(model, data, target)
+elapsed_time = time.time() - start
+
+scores = cv_results["test_score"]
+
+print(
+    "The mean cross-validation accuracy is: "
+    f"{scores.mean():.3f} ± {scores.std():.3f} "
+    f"with a fitting time of {elapsed_time:.3f}"
+)
+
+#Pipeline 3: One-hot encoding of categorical variables
+
+ 
