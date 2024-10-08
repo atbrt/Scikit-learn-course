06f10b361ad21b8dcc8e45e16fa5acd1139bd66e
Arold Toubert <aroldtoubert@gmail.com>
2024-10-08 19:33:12 +0200

    New pipeline

---
+++ Chapter 1/Exercise M1.05.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import time
+
+from sklearn.model_selection import cross_validate
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.compose import make_column_selector as selector
+
+adult_census = pd.read_csv("bases de données/adult-census.csv")
+
+target_name = "class"
+target = adult_census[target_name]
+data = adult_census.drop(columns=[target_name, "education-num"])
+
+numerical_columns_selector = selector(dtype_exclude=object)
+categorical_columns_selector = selector(dtype_include=object)
+numerical_columns = numerical_columns_selector(data)
+categorical_columns = categorical_columns_selector(data)
+
+#First pipeline: no numerical scaling and integer-coded categories
+categorical_preprocessor = OrdinalEncoder(
+    handle_unknown="use_encoded_value", unknown_value=-1
+)
+preprocessor = ColumnTransformer(
+    [("categorical", categorical_preprocessor, categorical_columns)],
+    remainder="passthrough",
+)
+
+model = make_pipeline(preprocessor, HistGradientBoostingClassifier())
+
+start = time.time()
+cv_results = cross_validate(model, data, target)
+elapsed_time = time.time() - start
+
+scores = cv_results["test_score"]
+
+print(
+    "The mean cross-validation accuracy is: "
+    f"{scores.mean():.3f} ± {scores.std():.3f} "
+    f"with a fitting time of {elapsed_time:.3f}"
+)
+
+#Second pipeline: scaling numerical features
